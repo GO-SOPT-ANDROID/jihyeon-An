f229a8008bf787df8b589ff6877d59e0badc6e9c
Jihyeon An <jh011007@naver.com>
2023-06-02 20:47:45 +0900

    Add : interceptor 구현

---
+++ app/src/main/java/org/go/sopt/interceptor/AuthInterceptor.kt
@@ -0,0 +1,16 @@
+package org.go.sopt.interceptor
+
+import okhttp3.Interceptor
+import okhttp3.Response
+
+class AuthInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val originRequest = chain.request()
+
+        val headerRequest = originRequest.newBuilder()
+            .header("Authorization", "")
+            .build()
+
+        return chain.proceed(headerRequest)
+    }
+}
